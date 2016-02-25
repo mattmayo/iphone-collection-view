75fd4a889b2d6ad20a22fcb21340e01bfae56b61
Matt Mayo <matt.mayo@fivestars.com>
2016-02-25 14:33:29 -0800

    Working collection view horizontal scrolling of cards.

--- iphone-collection-view/AppDelegate.swift
+++ iphone-collection-view/AppDelegate.swift
@@ -2,9 +2,7 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
-
     var window: UIWindow?
-
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         return true
---
+++ iphone-collection-view/CollectionViewController.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+private let collectionViewCellReuseIdentifier = "collection-view-cell-reuse-identifier"
+
+class CollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    let businesses = ["V-Cafe", "Sextant", "Super Que", "Vaper in Reverse", "Little Griddle"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.businesses.count
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCellWithReuseIdentifier(collectionViewCellReuseIdentifier, forIndexPath: indexPath) as? CollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.label.text = businesses[indexPath.row]
+    
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegateFlowLayout
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return 600
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return 600
+    }
+}
